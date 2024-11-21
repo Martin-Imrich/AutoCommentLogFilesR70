c781d498202ff5f8dfc51e0f0fbc27158e8cd6ea
Martin Imrich <martin.imrich@rieter.com>
2024-11-21 16:25:37 +0100

    Script that adds machine log comments:
    * Add comment to Debug_{0-4} type mesages

---
+++ dbglog_comment/dbglog_comment.py
@@ -0,0 +1,191 @@
+#
+# SU communicate with SI on messages Send_SUCSI_Ctrl_Msg:
+# Send_SUCSI_Ctrl_Msg: SUCSI msg 08, Data0 05, Data1 00 is send to sth Robot
+
+import os
+import re
+import sys
+
+from dbglog_map import Debug_0x00, Debug_0x01, Debug_0x02, Debug_0x03, Debug_0x04
+
+
+def get_debug_0_comment(substate: int) -> str:
+    """
+    Search for comment in Debug_0x00 dictionary.
+    #comment = f" <<< {Debug_0x00['substates']}, {sub['text']} >>>"
+    """
+    comment: str = ""
+
+    for sub in Debug_0x00['substates']:
+        if sub['code'] == substate:
+            comment = (
+                f" <<< {Debug_0x00['text']},"
+                f"     {sub['text']} >>>"
+            )
+            break
+
+    return comment
+
+
+def get_debug_1_comment() -> str:
+    """
+    Search for comment in Debug_0x01 dictionary.
+    """
+    comment: str = ""
+
+    comment = (
+        f" <<< {Debug_0x01['text']}, >>>"
+    )
+    return comment
+
+
+def get_debug_2_comment() -> str:
+    """
+    Search for comment in Debug_0x02 dictionary.
+    """
+    comment: str = ""
+    return comment
+
+
+def get_debug_3_comment() -> str:
+    """
+    Search for comment in Debug_0x03 dictionary.
+    """
+    comment: str = ""
+
+    comment = (
+        f" <<< {Debug_0x03['text']}, >>>"
+    )
+
+    return comment
+
+
+def get_debug_4_comment() -> str:
+    """
+    Search for comment in Debug_0x04 dictionary.
+    """
+    comment: str = ""
+    return comment
+
+    """
+        for state in MEvt_Info:
+            if state['id'] == main_state:
+                for sub in state['substates']:
+                    if sub['code'] == sub_state:
+                        return f" <<< {state['text']} >, < {sub['text']} >>>"
+        return ""
+    """
+
+
+def is_line_debug_message(line: str) -> bool:
+    """
+    Check if a line matches the debug message pattern.
+
+    Args:
+        line (str): The line to check.
+
+    Returns:
+        bool: True if the line matches the debug pattern, otherwise False.
+    """
+    debug_pattern = (
+        r"MEvt Info.*?Debug (?P<val1>0x[0-9a-fA-F]+)\s+"
+        r"(?P<val2>0x[0-9a-fA-F]+)\s+"
+        r"(?P<val3>0x[0-9a-fA-F]+)\s+"
+        r"(?P<val4>0x[0-9a-fA-F]+)"
+    )
+
+    return bool(re.search(debug_pattern, line))
+
+
+def get_debug_message_comment(line: str) -> str:
+    """
+    Extract data from debug message and return
+    the appropriate comment for a debug message
+    in the line.
+
+    Args:
+        line (str): The line containing the debug message.
+
+    Returns:
+        str: The comment for the debug message.
+    """
+    comment = ""
+
+    # r"MEvt Info.*?Debug (?P<val1>0x[0-9a-fA-F]+)\s+(?P<val2>0x[0-9a-fA-F]+)\s+(?P<val3>0x[0-9a-fA-F]+)\s+(?P<val4>0x[0-9a-fA-F]+)",
+    debug_pattern = (
+        r"MEvt Info.*?Debug (?P<val1>0x[0-9a-fA-F]+)\s+"
+        r"(?P<val2>0x[0-9a-fA-F]+)\s+"
+        r"(?P<val3>0x[0-9a-fA-F]+)\s+"
+        r"(?P<val4>0x[0-9a-fA-F]+)"
+    )
+
+    debug_match = re.search(debug_pattern, line)
+
+    if debug_match:
+        # Extract and convert values from the debug message
+        category = int(debug_match.group("val1"), 16)
+        data0 = int(debug_match.group("val2"), 16)
+        data1 = int(debug_match.group("val3"), 16)
+        data2 = int(debug_match.group("val4"), 16)
+
+        # Use a mapping for better scalability
+        if category == Debug_0x00['Debug_id']:
+            comment = get_debug_0_comment(data0)
+        elif category == Debug_0x01['Debug_id']:
+            comment = get_debug_1_comment()
+        elif category == Debug_0x02['Debug_id']:
+            comment = get_debug_2_comment()
+        elif category == Debug_0x03['Debug_id']:
+            comment = get_debug_3_comment()
+        elif category == Debug_0x04['Debug_id']:
+            comment = get_debug_4_comment()
+
+    if comment == "":
+        print(line)
+
+    return comment
+
+
+def modify_file(file_path) -> None:
+    """
+    Search for comment in Debug_0xXX dictionary.
+    """
+
+    # Read the file contents
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Modify the contents
+    modified_lines = []
+    for line in lines:
+        if is_line_debug_message(line):
+            line = line.strip() + get_debug_message_comment(line) + "\n"
+
+        modified_lines.append(line)
+
+    # Write the modified contents back to the same file
+    with open(file_path, 'w') as file:
+        file.writelines(modified_lines)
+
+
+# run command in notepad++ python:
+# python C:\Users\urimrm\Projekty\Scripts\AnalyzeRxLogFiles\dbglog_comment\dbglog_comment.py "$(FULL_CURRENT_PATH)"
+
+# fn = sys.argv[1]
+fn = 'C:/Users/urimrm/Projekty/Rx/Documentation to R70/MyNotesToRx/R70_Velen/02_Feeder_running/SUC002_01.txt'
+
+# option = sys.argv[2]  # Option
+option = "Debug_all"
+
+option_list = ["Debug_all", "Debug_0",
+               "Debug_1", "Debug_2",
+               "Debug_3", "Debug_4"]
+
+if option not in option_list:
+    option = "Debug_all"  # search in all dbglog_map dictionaries
+
+#
+
+if os.path.exists(fn):
+    print(fn)
+    modify_file(fn)
---
+++ dbglog_comment/dbglog_map.py
@@ -0,0 +1,174 @@
+# Debug shortcuts:
+# - AS(automatic service), PY(piecing to yarn), SY(seed yarn),
+# - MSL(Machine side left), MSR(Machine side right),
+# - ??
+# - BA? = AS: PY piecing failed with BA,
+
+
+# Example:
+# Nov 13 16: 28: 58 [INF] MEH LP: MEvt Info 17000(67) SI.3 SpP.61 Debug 0x00  0x2D 0x01 0x00 0x00
+# Nov 13 15: 39: 52 [INF] MEH LP: MEvt Info 20100(69) SUC.61 Debug 0x01  0x10 0x16 0x00 0x00 << < Technical Alarms >> >
+# Nov 13 16: 17: 41 [INF] MEH LP: MEvt Info 20100(69) SUC.61 Debug 0x02  0x1B 0x22 0x00 0x00
+# Nov 13 16: 20: 21 [INF] MEH LP: MEvt Info 17000(67) SI.3 SpP.61 Debug 0x03  0x02 0x00 0x01 0x2A
+# Nov 13 16: 28: 58 [INF] MEH LP: MEvt Info 20100(69) SUC.61 Debug 0x04  0x02 0x04 0x00 0x00
+
+# Definition files:
+# C: \Users\urimrm\Projekty\Rx\R70\SC_newAPI\SW\Common\ASW\MEH_DEF.h
+
+# Explanation:
+# Numbers:
+# - 17000(67) = MEH_SI_BASE
+# - 20100(69) = MEH_SUC_BASE
+
+
+Debug_0x00 = {
+    "Debug_id": 0x00,
+    "description": "None",
+    "const": "MEH___SI_STATE_CHG",
+    "text": "SUSI_STATE",  # SUSI_SUMA_STATE__
+    "substates": [
+            {'code': 0x00, 'text': "OK"},
+            {'code': 0x01, 'text': "UNIT_OPEN"},
+            {'code': 0x02, 'text': "YR"},
+            {'code': 0x03, 'text': "CUT_CONFIRM"},
+            {'code': 0x04, 'text': "REF_STARTPIECING"},
+            {'code': 0x05, 'text': "Unknown"},
+            #
+            {'code': 0x06, 'text': "Unknown"},
+            #
+            {'code': 0x20, 'text': "OFFS"},
+            {'code': 0x23, 'text': "HOMING"},
+            {'code': 0x24, 'text': "YARN_SEARCH"},
+            {'code': 0x25, 'text': "YARN_TRANSFER"},
+            {'code': 0x26, 'text': "YARN_PREPARATION"},
+            {'code': 0x27, 'text': "PIECING"},
+            {'code': 0x28, 'text': "SYNC"},
+            {'code': 0x29, 'text': "HANDOVER"},
+            {'code': 0x2A, 'text': "SPINNING"},
+            {'code': 0x2B, 'text': "ROBOFEED"},  # SUSI_SUMA_STATE__ROBOFEED
+            {'code': 0x2D, 'text': "CLEANING"},  # SUSI_SUMA_STATE__CLEANING
+            # 01: CLEANING_STARTED
+            # 02: CLEANING_ALMOST_FINISHED
+            # 03: CLEANING_READY
+            {'code': 0x2C, 'text': "DRAG_YARN_SEQ"},
+            {'code': 0x2E, 'text': "REDIpac"},
+            # SUSI_SUMA_STATE__MAN_SLIV_PREP
+            {'code': 0x2F, 'text': "MAN_SLIV_PREP"},
+            # 01: MAN_SLIV_PREP_STARTED
+            # 02: MAN_SLIV_PREP_ALMOST_FINISHED
+            # 03: MAN_SLIV_PREP_READY
+            {'code': 0x30, 'text': "SERVICE"},
+            # SUSI_SUMA_STATE__MEC_ROTOR_CLEAN -  Mechanical Rotor cleaning
+            {'code': 0x31, 'text': "Rotor cleaning - Without Robot"},
+            # 01: MEC_ROTOR_CLEAN_STARTED
+            # 02: MEC_ROTOR_CLEAN_ALMOST_FINISHED
+            # 03: MEC_ROTOR_CLEAN_READY
+            # SUSI_SUMA_STATE__WUF_CLEAN_SEQ
+            {'code': 0x32, 'text': "WUF_CLEAN_SEQ"},
+            {'code': 0x33, 'text': "MomMa Ready"},
+            {'code': 0x34, 'text': "SuMa Ready"},  # SuMa ready
+            {'code': 0x93, 'text': "STARTUP_PREPARED"},
+            {'code': 0x94, 'text': "STARTED_UP"},
+            {'code': 0x95, 'text': "OD_SWITCHED"},
+    ]
+}
+
+Debug_0x01 = {
+    'Debug_id': 0x01,
+    'description': "Automatically Confirmed Errors (messages in SI defined)",
+    'const': "MEH___SI_STATE_CHG",
+    'text': 'Autoconfirm at',
+    'substates': [
+        {'code': 0x00, 'text': "None",
+            'substates': [
+                {'code': 0x00, 'text': "None"},
+            ]
+         },
+        {'code': 0x01, 'text': "SUC",
+            'substates': [
+                {'code': 0x00, 'text': "Undefined"},
+            ]
+         },
+        {'code': 0x10, 'text': "BC, ",
+            'substates': [
+                {'code': 0x00, 'text': "Undefined"},
+                {'code': 0x16, 'text': "Sliver Feeding drive"},
+            ]
+         },
+        {'code': 0x70, 'text': "Robot or Rotor",
+            'substates': [
+                {'code': 0x00, 'text': "Undefined"},
+            ]
+         },
+        {'code': 0x90, 'text': "Robot Control",
+            'substates': [
+                {'code': 0x00, 'text': "Missing Signal from robot"},
+            ]
+         },
+        {'code': 0xA0, 'text': "No Valid Parameter",
+            'substates': [
+                {'code': 0x00, 'text': "Undefined"},
+            ]
+         },
+    ],
+}
+
+Debug_0x02 = {
+    'Debug_id': 0x02,
+    'description': "None",
+    'const': "?",
+    'text': 'None',
+    'substates': [
+            {'code': 0x00, 'text': "None",
+             'substates': [
+                 {'code': 0x00, 'text': "None"},
+             ]
+             },
+    ],
+}
+
+Debug_0x03 = {
+    'Debug_id': 0x03,
+    'description': "Statistics 0x03, 0x01 trial, 0x24 phase(piecing ends in phaers 24), Additional info",
+    'const': "?",
+    'text': 'Statistics',
+    'substates': [
+        {'code': 0x00, 'text': "None",
+            'substates': [
+                {'code': 0x00, 'text': "Undefined"},
+            ]
+         },
+    ],
+}
+
+Debug_0x04 = {
+    'Debug_id': 0x04,
+    'description': "?",
+    'const': "?",
+    'text': '?',
+    'substates': [
+        {'code': 0x00, 'text': "None",
+            'substates': [
+                {'code': 0x00, 'text': "Undefined"},
+            ]
+         },
+    ],
+}
+
+
+"""
+Example:
+Line 450: Nov 13 15:40:10 [INF] McCtrl BE: 33F03006 SC3>MC:   Emergency event! Code 0x30 0x06 // EMCY invalidate transition=(0x30), Transition ?? = 0x06
+Line 452: Nov 13 15:40:10 [INF] McCtrl BE: 33F03008 SC3>MC:   Emergency event! Code 0x30 0x08
+Line 453: Nov 13 15:40:10 [INF] McCtrl BE: 33F03008 SC3>MC:   Emergency event! Code 0x30 0x08
+Line 467: Nov 13 15:41:04 [INF] McCtrl BE: 33F03006 SC3>MC:   Emergency event! Code 0x30 0x06
+Line 468: Nov 13 15:41:04 [INF] McCtrl BE: 33F03008 SC3>MC:   Emergency event! Code 0x30 0x08 //
+Line 469: Nov 13 15:41:04 [INF] McCtrl BE: 33F03010 SC3>MC:   Emergency event! Code 0x30 0x10
+
+Emergency event! Code 0x30 0x06 // EMCY invalidate transition=(0x30), Transition 0x06 =  ??
+Emergency event! Code 0x30 0x08 // EMCY invalidate transition=(0x30), Transition 0x08 =  (neso s yarn run)
+Emergency event! Code 0x30 0x08
+Emergency event! Code 0x30 0x06
+Emergency event! Code 0x30 0x08 //
+Emergency event! Code 0x30 0x10 // EMCY invalidate transition=(0x30), State 0x10 = slave TA
+"""
